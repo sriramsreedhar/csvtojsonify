4501b4f50c444a66af9d8f693e8daa108cf01866
Sriram Sreedhar <sriramsreedhar003@gmail.com>
2024-04-25 21:47:44 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='csvtojsonify',
+    version='1.0.0',
+    packages=['csvtojsonify'],
+    entry_points={
+        'console_scripts': [
+            'csvtojsonify=csvtojsonify.csvtojsonify:main',
+        ],
+    },
+    install_requires=['csv', 'json'],
+    python_requires='>=3.10',
+    author='Sriram Sreedhar',
+    author_email='sriramsreedhar003@gmail.com',
+    description='A tool to convert CSV files to JSON.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sriramsreedhar/csvtojsonify',
+)
