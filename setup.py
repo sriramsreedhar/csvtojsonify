23c203dfae89f8e84e0abc9d8c991429309681d1
Sriram Sreedhar <sriramsreedhar003@gmail.com>
2024-04-25 22:18:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='csvtojsonify',
-    version='1.0.0',
+    version='1.2.0',
     packages=['csvtojsonify'],
     entry_points={
         'console_scripts': [
